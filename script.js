865b23d9fa5a1e5776939171954e822609c521e8
fdirige <118780727+fdirige@users.noreply.github.com>
2023-01-16 11:51:55 -0800

    Updated javascript with working code

--- script.js
+++ script.js
@@ -1,5 +1,59 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generateBtn");
+
+var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChar = "ABCDEFGHIJKLMNOPQESTUVWXYZ";
+var numericalChar = "0123456789";
+var specialChar = "!#$%&'()*+-./:;<=>?@[\^_`{|}~";
+
+function generatePassword() {
+  var passwordLength = prompt("Password length? Please enter a numberical value from 8-128.");
+  passwordLength = parseInt(passwordLength);
+  var password = ""
+  var passwordText = ""
+  if (passwordLength < 8) {
+    alert("Password must have more than 7 characters.");
+    return"";
+  }
+
+  if (passwordLength > 128) {
+    alert("Password must not have more than 128 characters.");
+    return"";
+  }
+
+  var lowercaseCharChoice = confirm("Would you like lowercase characters in your password?");
+
+  if (lowercaseCharChoice) {
+    passwordText += lowercaseChar;
+  }
+
+  var uppercaseCharChoice = confirm("Would you like uppercase characters in your password?");
+
+  if (uppercaseCharChoice) {
+    passwordText += uppercaseChar;
+  }
+
+  var numericalCharChoice = confirm("Would you like numerical characters in your password?");
+
+  if (numericalCharChoice) {
+    passwordText += numericalChar;
+  }
+
+  var specialCharChoice = confirm("Would you like special characters in your password?");
+
+  if (specialCharChoice) {
+    passwordText += specialChar;
+  }
+
+  for (var i = 0; i < passwordLength; i++) {
+    password += passwordText[Math.floor(Math.random() * passwordText.length)]
+  }
+
+
+console.log(password)
+
+  return password
+};
 
 // Write password to the #password input
 function writePassword() {
@@ -7,6 +61,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
 
 }
 
